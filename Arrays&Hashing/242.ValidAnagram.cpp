48f8ee6d5b293e8ed15479cb30048c87497d7dcb
bhomik005 <bhomik005@gmail.com>
2025-01-09 14:54:27 +1100

    code changes for 242.ValidAnagram.cpp

--- Arrays&Hashing/242.ValidAnagram.cpp
+++ Arrays&Hashing/242.ValidAnagram.cpp
@@ -1,39 +1,26 @@
+#include <string>
+
 class Solution {
-
-// anagram - a word or phrase formed by rearranging the letters of another word
 public:
-    bool isAnagram(string s, string t) {
+    // O(s + t) time - where s and t are string lengths
+    // O(1) space
+    bool isAnagram(std::string s, std::string t) {
 
+        // not having same length
         if(s.length() != t.length()) return false;
 
-        // O(m + n) time | O(1) space
-        // where m and n are the sizes of string s and t
-        int freqChars[26];
-
-        // O(m) time - where m is the length of s
+        // count the frequency of elements in string s
+        int count[26];
         for(int i = 0; i < s.length(); ++i) {
-            freqChars[s[i] - 'a']++;
+            count[s[i] - 'a']++;
         }
 
-        // O(n) time - where n is the length of t
+        // then check the frequency of elements in string t with string s
         for(int i = 0; i < t.length(); ++i) {
-            if(freqChars[t[i] - 'a'] > 0) freqChars[t[i] - 'a']--;
+            if(count[t[i] - 'a'] >= 1) count[t[i] - 'a']--;
             else return false;
         }
+
         return true;
-
-
-        // // base case - check the size of s and t
-        // // if the size is not equal - return false
-        // if(s.length() != t.length()) return false;
-
-        // // O(n log n) + O(m log m) - where n and m are the sizes of string s and t
-        // // naive - sort the strings and compare them
-        // // if they are equal - return true else return false
-        // sort(s.begin(), s.end());
-        // sort(t.begin(), t.end());
-        // if(s == t) return true;
-        // return false;
-
     }
 };
