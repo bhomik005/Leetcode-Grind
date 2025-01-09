a18d8e209bd402ea8230c72b86b7be7569decae5
bhomik005 <bhomik005@gmail.com>
2025-01-09 14:01:12 +1100

    code changes for 217.ContainsDuplicate.cpp

--- Arrays&Hashing/217.ContainsDuplicate.cpp
+++ Arrays&Hashing/217.ContainsDuplicate.cpp
@@ -1,32 +1,20 @@
-class Solution {
+#include <unordered_set>
+#include <vector>
 
+class Solution {
 public:
-    bool containsDuplicate(vector<int> &nums) {
-        // while iterating through the elements
-        // check if the curr element already exists in the set
-        // if it does - return true
-        // if not - save the element in the set
-        // O(n) time | O(n) space (Optimal solution)
-        unordered_set<int> set;
-        for (int i = 0; i < nums.size(); ++i) {
-            if (set.find(nums[i]) == set.end()) set.insert(nums[i]);
-            else return true;
+    // O(n) time  - where n is the size of nums
+    // O(n) space - where n is the size of nums
+    bool containsDuplicate(std::vector<int>& nums) {
+        std::unordered_set<int> set;
+        // start iterating over the elements
+        // and check if the element already exists in the set
+        // if yes - return true
+        // if no - store the element in the set and move on to the next element
+        for(unsigned int i = 0; i < nums.size(); ++i) {
+            if(set.find(nums[i]) != set.end()) return true;
+            else set.insert(nums[i]);
         }
         return false;
-
-        // // iterate through the elements multiple times
-        // // and check if any element occurs twice
-        // // O(n^2) time | O(1) time (Brute force solution)
-        // for(int i = 0; i < nums.size(); ++i) {
-        //     // O(n) times
-        //     for(int j = 0; j < nums.size(); ++j) {
-        //         if(i == j) continue;
-
-        //         // found a duplicate element
-        //         if(nums[i] == nums[j]) return true;
-        //     }
-        // }
-        // // if all the elements are distinct
-        // return false;
     }
 };
