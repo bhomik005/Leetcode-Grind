d122afe069ba9e6f7aa73d014c09bfe42f1cdca0
bhomik005 <bhomik005@gmail.com>
2025-01-09 14:43:03 +1100

    code changes for 1.TwoSum.cpp

--- Arrays&Hashing/1.TwoSum.cpp
+++ Arrays&Hashing/1.TwoSum.cpp
@@ -1,20 +1,20 @@
+#include <unordered_map>
+#include <vector>
 class Solution {
-
-// nums = [2, 7, 11, 15] , target = 9
 public:
-    vector<int> twoSum(vector<int>& nums, int target) {
-        unordered_map<int,int> prevNums;
+    // O(n) time  - where n is the size of nums
+    // O(n) space - where n is the size of nums
+    std::vector<int> twoSum(std::vector<int>& nums, int target) {
+        std::unordered_map<int,int> map;
 
-        // while iterating through the numbers, check if
-        // the difference between the target and the current
-        // num exists in the hashmap - if it does then
-        // return the value(idx) of the number in the
-        // map else store the number in the map with idx as val
-        // O(n) time | O(n) space
+        // start iterating over the elements
+        // calculate the difference between target and the nums[i]
+        // if the difference exists into the map - return the index and value of the key
+        // else insert the element into the hashmap
         for(int i = 0; i < nums.size(); ++i) {
-            int potentialMatch = target - nums[i];
-            if(prevNums.find(potentialMatch) == prevNums.end()) prevNums[nums[i]] = i;
-            else return {i, prevNums[potentialMatch]};
+            int difference = target - nums[i];
+            if(map.find(difference) != map.end()) return {i, map[difference]};
+            else map[nums[i]] = i;
         }
         return {};
     }
